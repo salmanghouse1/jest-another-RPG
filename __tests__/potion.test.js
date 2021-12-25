241abe4d74b7f6110cfec195db873752757e3867
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-12-25 15:40:54 -0500

    added potion

--- __tests__/potion.test.js
+++ __tests__/potion.test.js
@@ -1,11 +1,20 @@
-//10.1.7 
-const Potion = require('../lib/potion.js')
+const Potion = require('../lib/Potion.js')
 
 
-test('creates a health potion object', () => {
+test('Tests Potion', () => {
     const potion = new Potion('health');
-    expect(potion.name).toEqual(expect.any(string));
-    expect(potion.name.length).toBeGreaterThan(0);
 
+    expect(potion.name).toBe('health');
+
+    expect(potion.value).toEqual(expect.any(Number));
+
+})
+
+
+test('creates a random potion object', () => {
+    const potion = new Potion();
+
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
 });
